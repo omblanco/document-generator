b0045d98fb081004954d13943add669599545aa0
Oscar Martinez Blanco <omblanco@gmail.com>
2021-03-14 23:54:42 +0100

    Eureka Server fixed #5

---
+++ document-generator-eureka-server/src/test/java/com/omblanco/document/generator/eureka/DocumentGeneratorEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omblanco.document.generator.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentGeneratorEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- document-generator-exporter/src/main/java/com/omblanco/document/generator/exporter/DocumentGeneratorExporterApplication.java
+++ document-generator-exporter/src/main/java/com/omblanco/document/generator/exporter/DocumentGeneratorExporterApplication.java
@@ -2,7 +2,9 @@ package com.omblanco.document.generator.exporter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class DocumentGeneratorExporterApplication {
 
